fe28e7130c04704975ffd32de7c8d98c1b620c5d
magicharshit <harshit.py@gmail.com>
2013-10-16 01:22:38 +0530

    changed templateview to formview

--- core/views.py
+++ core/views.py
@@ -1,24 +1,21 @@
 from django.contrib.auth.forms import AuthenticationForm
 from django.template.context import RequestContext
-from django.views.generic.base import  View, TemplateView
 from django.contrib.auth import authenticate, login as auth_login
 from django.contrib.auth.models import User
 from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render_to_response
 from django.core.urlresolvers import reverse
-
+from django.views.generic.base import  View, TemplateView
+from django.views.generic.edit import FormView
 from .forms import RegistrationForm
 
+class EndUserRegistration(FormView):
+    template_name = 'signup.html'
+    form_class = RegistrationForm
+    success_url = '/'
 
-#todo use form view insead of template view/
-class EndUserRegistration(TemplateView):
-    template_name = ''
-
-    def post(self, request, *args, **kwargs):
-        registration_form = RegistrationForm(data=self.request.POST)
-        if registration_form.is_valid():
-            registration_form.save()
-            form = AuthenticationForm(data=request.POST)
-            auth_login(request, authenticate(username=request.POST['email'], password=request.POST['password1']))
-            #todo return to sucess page
-        #todo return to same page with errors
+    def form_valid(self, form):
+        # This method is called when valid form data has been POSTed.
+        # It should return an HttpResponse.
+        form.save()
+        return super(EndUserRegistration, self).form_valid(form)
