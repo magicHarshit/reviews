5e952af6e5768a04f88bc519ae290b0b4dee6d52
magicharshit <harshit.py@gmail.com>
2013-10-17 02:22:17 +0530

    added functionality for registration of admin

--- core/urls.py
+++ core/urls.py
@@ -1,7 +1,7 @@
 from django.conf.urls.defaults import patterns, url
 from django.contrib.auth.views import *
 
-from .forms import EndUserRegistration
+from .views import EndUserRegistration
 
 urlpatterns = patterns('',
         url(r'^$', EndUserRegistration.as_view(), name='signup'),
--- core/views.py
+++ core/views.py
@@ -7,15 +7,26 @@ from django.shortcuts import render_to_response
 from django.core.urlresolvers import reverse
 from django.views.generic.base import  View, TemplateView
 from django.views.generic.edit import FormView
+from django.contrib.auth import get_user_model
+
 from .forms import RegistrationForm
 
 class EndUserRegistration(FormView):
+    User = get_user_model()
     template_name = 'signup.html'
     form_class = RegistrationForm
     success_url = '/'
+
+    def get_context_data(self, **kwargs):
+        data = super(EndUserRegistration, self).get_context_data(**kwargs)
+        data['registration_form'] = data.get('form')
+        return data
 
     def form_valid(self, form):
         # This method is called when valid form data has been POSTed.
         # It should return an HttpResponse.
         form.save()
         return super(EndUserRegistration, self).form_valid(form)
+
+
+    
