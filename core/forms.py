fd274dbc81d0262b7013732165f99473c9614206
magicharshit <harshit.py@gmail.com>
2013-10-17 02:21:33 +0530

    remove userCreationForm

--- core/forms.py
+++ core/forms.py
@@ -5,16 +5,16 @@ from django.contrib.auth import get_user_model
 from django.contrib.auth.forms import  AuthenticationForm
 
 
-class RegistrationForm(UserCreationForm):
+class RegistrationForm(forms.ModelForm):
     """
      Base class for  users creation. Extend this to get a form that accepts
     user parameters for new user  creation
     """
 
-    email = forms.CharField(label=_("Username"), max_length=30,
+    username = forms.EmailField(label=_("Email"), max_length=30,
         help_text =_("Required. 30 characters or fewer. Letters, digits and @/./+/-/_ only."),
         error_messages ={'invalid': _("Enter a valid email address"), },
-        widget=forms.TextInput(attrs={'placeholder': 'Username'}))
+        widget=forms.TextInput(attrs={'placeholder': 'Email id'}))
     password1 = forms.CharField(label=_("Password"), widget=forms.PasswordInput(attrs={'placeholder':'Password'}))
     password2 = forms.CharField(label=_("Password confirmation"), widget=forms.PasswordInput(attrs={'placeholder':'Re-enter Pasword'}),
     help_text =_("Enter the same password as above, for verification."))
@@ -26,11 +26,11 @@ class RegistrationForm(UserCreationForm):
 
     class Meta:
         model = get_user_model()
-        fields = ('email', 'first_name', 'last_name', 'password1', 'password2', 'organisation')
+        fields = ('username', 'first_name', 'last_name', 'password1', 'password2', 'organisation')
 
     def clean_password1(self):
         password1 = self.cleaned_data["password1"]
-        if len(password1) < 10:
+        if len(password1) < 5:
             raise forms.ValidationError("Enter a password of min 10 charcters")
         return password1
 
